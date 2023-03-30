2b1d9025ea4ab18ada3cbe70543fcff385d553da
Katlego J Mogashoa <106239836+katGhost@users.noreply.github.com>
2023-03-30 12:09:40 +0200

    introduction page

---
+++ lib/welcome_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import './pages/home_page.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Column(
+        children: [
+          // first its the LOGO (image or icon) at the top
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 80, right: 80, bottom: 40, top: 120),
+            child: Image.asset('lib/images/egg.png'),
+          ),
+
+          // second part will be the welcome message
+          const Padding(
+            padding: EdgeInsets.all(25),
+            child: Text(
+              "We deliver quality foods right at your doorstep",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+            ),
+          ),
+
+          //followed by its subtitle
+          Text(
+            'We will take care of your health',
+            textAlign: TextAlign.center,
+            style: TextStyle( fontSize: 16, fontWeight: FontWeight.w400, color: Colors.grey[500]),
+          ),
+
+          const Spacer(),
+
+          //last will be the -Get Started- button
+
+          GestureDetector(
+            onTap: () => {},
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                  color: Colors.tealAccent[400],
+                  borderRadius: BorderRadius.circular(12)),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black87,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+
+          const Spacer(),
+        ],
+      )),
+    );
+  }
+}
