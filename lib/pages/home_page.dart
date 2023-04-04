ee49f3cfb6a624ca384ee3349bcd49250125f02e
Katlego J Mogashoa <106239836+katGhost@users.noreply.github.com>
2023-04-04 12:50:33 +0200

    images added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_app/model/cart_model.dart';
+import 'package:grocery_app/welcome_page.dart';
 import '../components/grocery_item_tile.dart';
 import 'package:provider/provider.dart';
+
+import 'cart_page.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,21 +14,116 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /*
+         if drawer does not open and gives ERROR: Exception caught by gesture====
+         ===Scaffold.of() called with a context that does not contain a Scaffold.=====
+         -- Then wrap the Drawer caller/ button with a Builder.
+       */
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu_rounded,
+              color: Colors.black,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          }
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          children: [
+            // links to other pages (cart page, individual item view page and logout)
+            const DrawerHeader(
+              child: Icon(Icons.food_bank_rounded,
+              color: Colors.grey,
+              size: 60,
+            ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Divider(
+                color: Colors.grey,
+              ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.home_rounded,
+                  color: Colors.grey,
+                  size: 24,
+                ),
+                title: Text('Home',
+                style: TextStyle(
+                  color: Colors.grey
+                ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: ListTile(
+                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const CartPage();
+                })),
+                leading: const Icon(Icons.shopping_bag_rounded,
+                  color: Colors.grey,
+                  size: 24,
+                ),
+                title: const Text('Cart',
+                style: TextStyle(
+                  color: Colors.grey
+                ),
+                ),
+              ),
+            ),
+
+            const Spacer(), // pushes the item below to the bottom of the page
+
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+              child:  ListTile(
+                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const WelcomePage();
+                })),
+                leading: const Icon(Icons.logout_rounded,
+                  color: Colors.grey,
+                  size: 24,
+                ),
+                title: const Text('Logout',
+                style: TextStyle(
+                  color: Colors.grey
+                ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              const SizedBox(
-                height: 40,
-              ),
+              // const SizedBox(
+              //   height: 40,
+              // ),
               // greeting message text
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text("Good to see you sir!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                    )),
-              ),
+              // const Padding(
+              //   padding: EdgeInsets.symmetric(horizontal: 24.0),
+              //   child: Text("Good to see you sir!",
+              //       style: TextStyle(
+              //         fontWeight: FontWeight.w500,
+              //       )),
+              // ),
 
               const SizedBox(
                 height: 6,
